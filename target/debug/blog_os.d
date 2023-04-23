3cc8bd50c3900136772301f640abd6db51acbee5
haha <email>
2023-04-23 16:47:32 +0800

    .

---
+++ target/debug/blog_os.d
@@ -0,0 +1,1 @@
+C:\Users\bert\Desktop\rustos\target\debug\blog_os.exe: C:\Users\bert\Desktop\rustos\src\main.rs
---
+++ target/debug/deps/blog_os.d
@@ -0,0 +1,5 @@
+C:\Users\bert\Desktop\rustos\target\debug\deps\blog_os.exe: src\main.rs
+
+C:\Users\bert\Desktop\rustos\target\debug\deps\blog_os.d: src\main.rs
+
+src\main.rs:
