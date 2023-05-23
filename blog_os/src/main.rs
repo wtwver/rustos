90125122097b17092607edeb93e095bfaddf381c
Your Name <you@example.com>
2023-05-23 14:29:36 +0800

    str

--- blog_os/src/main.rs
+++ blog_os/src/main.rs
@@ -44,17 +44,16 @@ macro_rules! println {
     // ($($arg:tt)*) => (print($($arg)*));
 }
 
-fn _print(name: &str, color: u8) {
+fn _print(name: &str, name2: &str, color: u8) {
     let vga_buffer = 0xb8000 as *mut u8;
 
     let name_b = name.as_bytes();
-    let name_b2 = "1".as_bytes();
+    let name_b2 = name2.as_bytes();
     // name_b = name_b + name_b;
 
     let mut buffer: [u8; 100] = [0; 100];
     buffer[0..name_b.len()].copy_from_slice(name_b);
-    buffer[name_b.len()..2].copy_from_slice(name_b2);
-    // buffer[name_b2.len()..].copy_from_slice(name_b2);
+    buffer[name_b.len()..name_b.len() + name_b2.len()].copy_from_slice(name_b2);    // buffer[name_b2.len()..].copy_from_slice(name_b2);
 
     for (i, &byte) in buffer.iter().enumerate() {
         unsafe {
@@ -67,7 +66,7 @@ fn _print(name: &str, color: u8) {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // let String global = "Hello " + "GG" ;
-    _print("g", 0xa);
+    _print("Hello ", "GGasdasdsadsadasdas",0xa);
 
     loop {}
 }
